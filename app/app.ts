6ceb7896148ff1ebb0d702739833420e0f212f08
Rodrigo Real <rodrigoreal@live.com>
2016-05-15 21:06:55 -0300

    Initial commit

---
+++ app/app.ts
@@ -0,0 +1,20 @@
+import {App, Platform} from 'ionic-angular';
+import {StatusBar} from 'ionic-native';
+import {TabsPage} from './pages/tabs/tabs';
+
+
+@App({
+  template: '<ion-nav [root]="rootPage"></ion-nav>',
+  config: {} // http://ionicframework.com/docs/v2/api/config/Config/
+})
+export class MyApp {
+  rootPage: any = TabsPage;
+
+  constructor(platform: Platform) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+    });
+  }
+}
