9f6b90aabd1ae02c5c53fd2b4db8413b4abff92a
Rodrigo Real <rodrigoreal@live.com>
2016-05-15 21:12:51 -0300

    Added angularfire2 and firebase

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,1 +1,2 @@
 /// <reference path="browser/ambient/es6-shim/index.d.ts" />
+/// <reference path="browser/ambient/firebase/index.d.ts" />
--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,1 +1,2 @@
 /// <reference path="main/ambient/es6-shim/index.d.ts" />
+/// <reference path="main/ambient/firebase/index.d.ts" />
