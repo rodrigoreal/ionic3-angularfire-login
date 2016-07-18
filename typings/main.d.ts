944ddbe35e7bc87f6465ada0ecf8dbe098064766
Rodrigo Real <rodrigoreal@live.com>
2016-07-18 07:46:50 -0300

    fix(typings): firebase typing errors

--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,2 +1,2 @@
 /// <reference path="main/ambient/es6-shim/index.d.ts" />
-/// <reference path="main/ambient/firebase/index.d.ts" />
+/// <reference path="../node_modules/angularfire2/firebase3.d.ts"/>
